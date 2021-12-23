521b7e9aee6479e91f415077aa884e83acf16d6b
castillios <93962560+castillios@users.noreply.github.com>
2021-12-22 18:36:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ PRESETS = [
 
 MATCHES = []
 
-user_profile = None
+USER_PROF = []
+
 
 def create_profile() -> Profile:
     user_name = input('Enter your name:\n')
@@ -39,7 +40,14 @@ def create_profile() -> Profile:
 
     user_profile = Profile(user_name, user_age, user_occupation, user_tagline)
 
-    return user_profile
+    USER_PROF.append(user_profile)
+
+    return 'Profile created'
+
+
+def del_prof():
+    USER_PROF.pop(0)
+
 
 # 'graphical' user interface
 def display_profile(prof:Profile):
@@ -49,7 +57,6 @@ def display_profile(prof:Profile):
     print(f"\n{x:@^20}")
  
     print(f"\n\n{face}\n\n")
-
     
     print(prof.name)
     print(f"{prof.age} * {prof.occupation}")
@@ -70,8 +77,55 @@ def ascii_face():
     face = f"{hair:^20}\n{eyes:^20}\n{nose:^20}\n{mouth:^20}"
     return face
 
+
+def main_menu():
+    title = "PenPal"
+    spacer_1 = '@' * 20
+    menu = f"\n{spacer_1:^20}\n{title:^20}\n{spacer_1:^20}\n\n{'browse (b)':^20}\n{'matches (m)':^20}\n{'my profile (p)':^20}\n{'create profile (c)':^20}\n{'quit (q)':^20}\n\n{spacer_1}\n"
+
+    return menu
+
+
+def user_input(msg:str=''):
+    user_in = input(msg)
+    user_in = user_in.lower()
+    return user_in
+
 if __name__ == "__main__":
     # me = create_profile()
     # print(me)
-    display_profile(PRESETS[1])
+    print(main_menu())
+    while True:
+        option = user_input('Test: ')
+        if option == 'q':
+            print()
+            print()
+            print('Quitting...')
+            break
+
+        if option == 'b':
+            print('not implemented')
+        elif option == 'm':
+            print('not implemented')
+        elif option == 'p':
+            if len(USER_PROF) == 0:
+                while True:
+                    create_true = user_input('No existing profile. Create new profile? (y/n)\n')
+                    if create_true == 'y':
+                        user = create_profile()
+                        break
+                    elif create_true == 'n':
+                        break
+                    else:
+                        print("Invalid input! Type 'y' or 'n'.")
+            else:
+                display_profile(USER_PROF[0])
+        elif option == 'c':
+            if len(USER_PROF) >= 1:
+                print("A user profile already exists. Create a new one? (y/n)")
+                print("WARNING: This will delete the existing profile!")
+                test = user_input()
+            else:
+                new_user = create_profile()
+
     
